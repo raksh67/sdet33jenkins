ba09422fd9638a4ecc4278fd916d20f664c27723
USER <USER@DESKTOP-B5SU5KF>
2022-04-14 13:06:15 +0530

    first commit

---
+++ src/test/java/com/jenkins/Class1Test.java
@@ -0,0 +1,11 @@
+package com.jenkins;
+
+import org.testng.annotations.Test;
+
+public class Class1Test {
+@Test
+public void test1() {
+	System.out.println("START JENKINS ");
+	
+}
+}
---
+++ src/test/java/com/jenkins/Class2Test.java
@@ -0,0 +1,11 @@
+package com.jenkins;
+
+import org.testng.annotations.Test;
+
+public class Class2Test {
+@Test
+public void test2() {
+	System.out.println("TESTING JENKINS");
+}
+
+}
