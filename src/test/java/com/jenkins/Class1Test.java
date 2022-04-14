718b87d1ff1febcfc65f41825d68dcf1d51d4fc8
USER <USER@DESKTOP-B5SU5KF>
2022-04-14 17:44:36 +0530

    added getproperty

--- src/test/java/com/jenkins/Class1Test.java
+++ src/test/java/com/jenkins/Class1Test.java
@@ -5,6 +5,9 @@ import org.testng.annotations.Test;
 public class Class1Test {
 @Test
 public void test1() {
+	String browser = System.getProperty("BROWSER");
+	String url = System.getProperty("URL");
+	System.out.println(browser+"   "+url);
 	System.out.println("START JENKINS ");
 	
 }
